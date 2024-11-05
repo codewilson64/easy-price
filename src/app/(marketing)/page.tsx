7b3002fde54291336652bd822be3dc4cdca97cfa
codewilson64 <wilsongambit@gmail.com>
2024-11-05 20:29:40 +0700

    Add pricing card

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,6 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { ReactNode } from "react";
+import Link from "next/link";
 import { SignUpButton } from "@clerk/nextjs";
-import { ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon, CheckIcon } from "lucide-react";
+import { ClerkIcon } from "./_icons/Clerk";
+import { NeonIcon } from "./_icons/Neon";
+import { subscriptionTiersInOrder } from "@/data/subscriptionTiers";
+import { formatCompactNumber } from "@/lib/formatters";
+
+// Shadcn
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
 export default function HomePage() {
   return (
@@ -19,6 +29,112 @@ export default function HomePage() {
           </Button>
         </SignUpButton>
       </section>
+
+      <section className="bg-primary text-primary-foreground">
+        <div className="container py-16 flex flex-col gap-16 px-8 md:px-16">
+          <h2 className="text-3xl text-center text-balance font-semibold">
+            Trusted by the top modern companies
+          </h2>
+          <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-16">
+            <Link href="https://clerk.com">
+              <ClerkIcon />
+            </Link>
+            <Link href="https://neon.tech">
+              <NeonIcon />
+            </Link>
+            <Link href="https://clerk.com">
+              <ClerkIcon />
+            </Link>
+            <Link href="https://neon.tech">
+              <NeonIcon />
+            </Link>
+            <Link href="https://clerk.com">
+              <ClerkIcon />
+            </Link>
+            <Link href="https://neon.tech">
+              <NeonIcon />
+            </Link>
+            <Link href="https://clerk.com">
+              <ClerkIcon />
+            </Link>
+            <Link href="https://neon.tech">
+              <NeonIcon />
+            </Link>
+            <Link href="https://clerk.com">
+              <ClerkIcon />
+            </Link>
+            <Link className="md:max-xl:hidden" href="https://neon.tech">
+              <NeonIcon />
+            </Link>
+          </div>
+        </div>        
+      </section>
+
+      <section id="pricing" className="px-8 py-16 bg-accent/5">
+        <h2 className="text-4xl text-center text-balance font-semibold mb-8">
+          Pricing software which pays for itself 20x over
+        </h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto">
+          {subscriptionTiersInOrder.map(tier => (
+            <PricingCard key={tier.name} {...tier}/>
+          ))}
+        </div>
+      </section>
     </>
+  )
+}
+
+function PricingCard({
+  name,
+  priceInCents,
+  maxNumberOfVisits,
+  maxNumberOfProducts,
+  canRemoveBranding,
+  canAccessAnalytics,
+  canCustomizeBanner,
+}: (typeof subscriptionTiersInOrder)[number]) {
+  const isMostPopular = name === "Standard"
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="text-accent font-semibold mb-6">{name}</div>
+        <CardTitle className="text-xl font-bold">${priceInCents / 100} /mo</CardTitle>
+        <CardDescription>
+          {formatCompactNumber(maxNumberOfVisits)} pricing page visits/mo
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <SignUpButton>
+          <Button className="text-lg w-full rounded-lg" variant={isMostPopular ? "accent" : "default"}>Get Started</Button>
+        </SignUpButton>
+      </CardContent>
+
+      <CardFooter className="flex flex-col gap-4 items-start">
+        <Feature className="font-bold">
+          {maxNumberOfProducts}{" "}
+          {maxNumberOfProducts === 1 ? "product" : "products"}
+        </Feature>
+        <Feature>PPP discounts</Feature>
+        {canAccessAnalytics && <Feature>Access analytics</Feature>}
+        {canRemoveBranding && <Feature>Remove Easy PPP branding</Feature>}
+        {canCustomizeBanner && <Feature>Banner customization</Feature>}
+      </CardFooter>
+    </Card>
+  )
+}
+
+function Feature({
+  children,
+  className,
+}: {
+  children: ReactNode
+  className?: string
+}) {
+  return (
+    <div className={cn("flex items-center gap-2", className)}>
+      <CheckIcon className="size-4 stroke-accent bg-accent/25 rounded-full p-0.5"/>
+      <span>{children}</span>
+    </div>
   )
 }
